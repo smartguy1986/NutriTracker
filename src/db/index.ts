5660bac17c4f24d1ac695a6248def11f06e5f52c
Arijit Nandi <arijit.nandi@netenrich.com>
2026-08-24 02:10:42 +0530

    Fixing db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,16 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL!;
+import { getConnectionString } from '@netlify/database';
+
+// @ts-ignore
+let connectionString = process.env.DATABASE_URL;
+try {
+  connectionString = getConnectionString();
+} catch (error) {
+  // Fall back to DATABASE_URL locally
+}
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false });
+export const client = postgres(connectionString!, { prepare: false });
 export const db = drizzle(client, { schema });
